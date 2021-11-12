91ba131f86ee7e50c5dd2247e415643aee44fd1e
arne-sas <r0802226@student.thomasmore.be>
2021-11-12 13:38:50 +0100

    app.py working

--- nbs/app.py
+++ nbs/app.py
@@ -3,79 +3,57 @@
 
 # ## Imports
 
-# In[25]:
+# In[7]:
 
 
 from pycaret.regression import *
 import pandas as pd 
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 import streamlit as st
 from PIL import Image
 import os
+import pickle
+
+#Open model created by the notebook
+model = pickle.load(open('../model/box_office_model.pkl','rb'))
+
+#create main page
+def main():
+    image = Image.open('../assets/Box_Office.jfif')
+    st.image(image, use_column_width=False)
 
 
-# ## Streamlit
-
-# In[26]:
-
-
+    add_selectbox = st.sidebar.selectbox('How would you like to predict?', ('Online', 'Batch')) #bruke batch for aa predikere paa alle bildene. 
+    st.sidebar.info('This app is created to predict revenue for movies' )
+    st.sidebar.success('DAT158')
+    st.title('Box Office Predictions')
     
-    """ def __init__(self):
-        self.model = load_model('../model/box_office_model') 
-        self.save_fn = '../path.csv' 
+
+    if add_selectbox == 'Online': 
+        budget = st.number_input('budget', min_value=0, max_value=1000000000, value=1000000)
+        popularity = st.number_input('popularity', min_value=0., max_value=100., value=0., format="%.2f", step=1.)
+        runtime = st.number_input('runtime', min_value=0., max_value=500., value=0., format="%.2f", step=1.)
         
-    def predict(self, input_data): 
-        return predict_model(self.model, data=input_data)
-    
-    def store_prediction(self, output_df): 
-        if os.path.exists(self.save_fn):
-            save_df = pd.read_csv(self.save_fn)
-            save_df = save_df.append(output_df, ignore_index=True)
-            save_df.to_csv(self.save_fn, index=False)
-            
-        else: 
-            output_df.to_csv(self.save_fn, index=False) 
-            
+        inputs = [[budget,runtime,popularity]]
+        inputs_scaled = StandardScaler().fit_transform(inputs)
 
-    
-    def run(self):
-        image = Image.open('../assets/Box_Office.jfif')
-        st.image(image, use_column_width=False)
-    
-    
-        add_selectbox = st.sidebar.selectbox('How would you like to predict?', ('Online', 'Batch')) #bruke batch for aa predikere paa alle bildene. 
-        st.sidebar.info('This app is created to predict revenue for movies' )
-        st.sidebar.success('DAT158')
-        st.title('Box Office Predictions')
+        if st.button('Predict'): 
+            result = model.predict(inputs_scaled)
+            format_result = "{:.2f}".format(float(result))
+            print(format_result)
+            st.success('Predicted output: €{:,.2f}'.format(float(result)))
+            
         
-       
-        if add_selectbox == 'Online': 
-            budget = st.number_input('budget', min_value=0, max_value=100000000, value=1000000)
-            popularity = st.number_input('popularity', min_value=0, max_value=100, value=0)
-            runtime = st.number_input('runtime', min_value=0, max_value=500, value=0)
-            
-            output=''
-            input_dict = {'budget':budget, 'popularity':popularity, 'runtime':runtime}
-            input_df = pd.DataFrame(input_dict, index=[0])
         
-            if st.button('Predict'): 
-                output = self.predict(input_df)
-                self.store_prediction(output)
-                
-                output = output['Label'][0]
-                #output = str(output['Label'])
-                
-            
-            st.success('Predicted output: {}'.format(output))
-            
-        if add_selectbox == 'Batch': 
-            fn = st.file_uploader("Upload csv file for predictions") #st.file_uploader('Upload csv file for predictions, type=["csv"]')
-            if fn is not None: 
-                input_df = pd.read_csv(fn)
-                predictions = self.predict(input_df)
-                st.write(predictions)         """
-            
-# In[24]:
+    if add_selectbox == 'Batch': 
+        fn = st.file_uploader("Upload csv file for predictions") #st.file_uploader('Upload csv file for predictions, type=["csv"]')
+        if fn is not None: 
+            input_df = pd.read_csv(fn)
+            predictions = self.predict(input_df)
+            st.write(predictions)
 
-
+#Start application
+if __name__ =='__main__':
+  main()
 
